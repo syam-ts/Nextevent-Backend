f5e31b975277c347779985a0757d9d52c646ca71
syam-ts <syamnandhu3@gmail.com>
2025-07-19 16:28:17 +0530

    chore: cors cofig updated for new frontend url

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,22 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { indexRouter } from "./presentation/exress-http/routes";
 import { ConnectDB } from "./infrastructure/database/db";
-import morgan from 'morgan'
-import cors from 'cors'
+import morgan from "morgan";
+import cors from "cors";
 dotenv.config({
     path: ".env",
 });
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
-app.use(morgan("dev"));  
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        methods: "GET,POST,PUT,PATCH",
+    })
+);
+app.use(morgan("dev"));
 
 app.use("/api/v1", indexRouter);
 
@@ -21,6 +26,6 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
     await ConnectDB();
-     console.log('Database Connected Successfully')
+    console.log("Database Connected Successfully");
     console.log(`Server listening to port ${port}`);
 });
