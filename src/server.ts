4be1768883d1a512dd1b9e51f13261bd6c402a10
syam-ts <syamnandhu3@gmail.com>
2025-07-21 18:40:29 +0530

    chore: db config modified

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,32 @@
 import express, { Express } from "express";
-require('module-alias/register');
+require("module-alias/register");
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { indexRouter } from "./presentation/exress-http/routes";
-import { ConnectDB } from "./infrastructure/database/db";
+import { indexRouter } from "./presentation/exress-http/routes"; 
 import morgan from "morgan";
 import cors from "cors";
+import ConnectDB from "./infrastructure/database/db";
 // import userRouter from "./presentation/exress-http/__tests__/userRouter";
 
-export class Server {
+const connectDB = new ConnectDB();
+
+class Server {
+
     private app: Express;
     private port: number;
+    private frontendUrl: string;
+    private corsMethods: string[];
 
     constructor() {
+
         dotenv.config({
             path: ".env",
         }),
-            (this.app = express());
+        
+        this.app = express();
         this.port = parseInt(process.env.PORT || "3000");
+        this.frontendUrl = process.env.FRONTEND_URL as string;
+        this.corsMethods = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"];
 
         this.configureMiddlewares();
         this.configuredRoute();
@@ -30,8 +39,8 @@ export class Server {
         this.app.use(cookieParser());
         this.app.use(
             cors({
-                origin: process.env.FRONTEND_URL,
-                methods: "GET,POST,PUT,PATCH",
+                origin: this.frontendUrl,
+                methods: this.corsMethods,
                 credentials: true,
             })
         );
@@ -43,14 +52,14 @@ export class Server {
     }
 
 
-    public configuredRoute(): void {
+    private configuredRoute(): void {
         this.app.use("/api/v1", indexRouter);
         // this.app.use('/api/v1/users', userRouter) // for testing only
     }
 
 
     private async connectToDatabase(): Promise<void> {
-        await ConnectDB();
+        await connectDB.connect();
 
         console.log("Database Connected Successfully");
     }
@@ -59,7 +68,12 @@ export class Server {
     public start(): void {
         this.app.listen(this.port, async (): Promise<void> => {
             this.connectToDatabase();
-            console.log(`Server listening to port ${this.port}`);
+            console.log(
+                `Server listening to port ${this.port
+                } on ${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()} `
+            );
         });
     }
 }
+
+export default Server;
