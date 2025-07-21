832503dbc6a61a3d44ea8bc8e6748667d51c2405
syam-ts <syamnandhu3@gmail.com>
2025-07-21 17:20:20 +0530

    fix: test command fix

--- src/presentation/exress-http/__tests__/userRouter.ts
+++ src/presentation/exress-http/__tests__/userRouter.ts
@@ -1,11 +1,11 @@
-import express from "express"; 
-import { User } from "../../../testing/Schema/User";
+// import express from "express"; 
+// import { User } from "../../../testing/Schema/User";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const users = await User.find();
-  res.status(200).json(users);
-});
+// router.get("/", async (req, res) => {
+//   const users = await User.find();
+//   res.status(200).json(users);
+// });
 
-export default router;
+// export default router;
