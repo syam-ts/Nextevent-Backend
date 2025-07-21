21274bf86dab5770412653e6a13b7d8ed38709b1
syam-ts <syamnandhu3@gmail.com>
2025-07-21 12:59:13 +0530

    feat: add refresh-token api

--- src/presentation/exress-http/routes/userRoute.ts
+++ src/presentation/exress-http/routes/userRoute.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UserController } from "../../controllers/userController"; 
 import { verifyToken } from "../../middlewares/verifyToken";
+import refreshToken from "../../../utils/jwt/refreshToken";
 
 const userRouter = Router();
 const userController = new UserController();
@@ -14,11 +15,12 @@ const {
     getSingleClient,
 } = userController;
 
-userRouter.post("/signup", signupUser);
-userRouter.post("/login", loginUser);
+userRouter.get('/refresh-token', refreshToken);
 userRouter.get("/profile", verifyToken, getUserProfile);
 userRouter.get("/my-clients", verifyToken, getMyClients);
 userRouter.get("/client/:clientId", verifyToken, getSingleClient);
+userRouter.post("/signup", signupUser);
+userRouter.post("/login", loginUser);
 userRouter.put("/update", verifyToken, updateUser);
 
 export default userRouter;
